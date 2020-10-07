3afe052f509b9d54b791c40e8295df244b94a20a
Nick <hungnd.iam@gmail.com>
2020-10-07 14:32:13 +0700

    Init

---
+++ src/test/java/com/nick/tempaz/TempAzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.tempaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempAzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
